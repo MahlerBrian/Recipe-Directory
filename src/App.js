de9d519b585973f908c2035cf1b5ae23e381f019
Brian Albert <brian.david.albert@gmail.com>
2022-10-15 09:22:03 -0400

    installed React Router, created pages root folder and components subfolders, imported components and component style sheets

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+//page components
+import Home from './pages/home/home'
+import Create from './pages/create/create'
+import Search from './pages/search/search'
+import Recipe from './pages/recipe/recipe'
+
+//styles
 import './App.css'
 
 function App() {
